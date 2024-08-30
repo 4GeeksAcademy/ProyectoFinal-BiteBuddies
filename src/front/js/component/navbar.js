114fcf0d8eb7b10e8ad16bcf89f6104c317da0c4
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-08-30 09:20:59 +0000

    Navbar hecha

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,6 +7,7 @@ export const Navbar = () => {
 	const location = useLocation();
     const { store, actions } = useContext(Context);
     const [search, setSearch] = useState("");
+    const currentPath = location.pathname;
 
     useEffect(() => {
         actions.traerIngredientes();
@@ -43,108 +44,61 @@ export const Navbar = () => {
     };
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-            {location.pathname === "/login"&&(
-                <div className="container-fluid d-flex justify-content-between">
-                    <div>
-                        <Link to="/">
-                            <span className="navbar-brand m-1 h1">HOME</span>
+<nav className="navbar navbar-light bg-light">
+<div className="container-fluid">
+    <Link to="/">
+        <span className="navbar-brand mb-0 h1">HOME</span>
+    </Link>
+
+    {currentPath === "/login" || currentPath === "/sign_in" || currentPath.startsWith("/user-profile") ? (
+        <h1>Cocinar como en casa</h1>
+    ) : (
+        <div className="m-1 w-50" style={{ position: "relative" }}>
+            <form onSubmit={handleSearchSubmit}>
+                <input
+                    type="text"
+                    className="form-control me-2"
+                    placeholder="Buscar ingredientes..."
+                    value={search}
+                    onChange={handleSearchChange}
+                />
+                {renderDropdown()}
+            </form>
+        </div>
+    )}
+
+    <div className="ml-auto">
+        {store.isLoggedIn && store.currentUser ? (
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    {store.currentUser.user_name || "error"}
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li>
+                        <Link className="dropdown-item" to={`/user-profile/${store.currentUser.id}`}>
+                            Perfil
                         </Link>
-                    </div>
-                    <div>
-                        <h1>Cocinar como en casa</h1>
-                    </div>
-                    <div>
-                        <Link to="/login">
-                                <button className="btn btn-primary m-1">LOGIN</button>
-                            </Link>
-                            <Link to="/sign_in">
-                                <button className="btn btn-primary m-1">SIGN IN</button>
-                            </Link>
-                    </div>
-                        
-
-                </div>
-
-            )}
-            {location.pathname === "/sign_in"&&(
-                <div className="container-fluid d-flex justify-content-between">
-                    <div>
-                        <Link to="/">
-                            <span className="navbar-brand m-1 h1">HOME</span>
+                    </li>
+                    <li><a className="dropdown-item" href="#">Favoritos</a></li>
+                    <li>
+                        <Link className="dropdown-item" to={`/login`} onClick={actions.logout}>
+                            Logout
                         </Link>
-                    </div>
-                    <div>
-                        <h1>Cocinar como en casa</h1>
-                    </div>
-                    <div>
-                        <Link to="/login">
-                                <button className="btn btn-primary m-1">LOGIN</button>
-                            </Link>
-                            <Link to="/sign_in">
-                                <button className="btn btn-primary m-1">SIGN IN</button>
-                            </Link>
-                    </div>
-                        
-
-                </div>
-
-            )}
-            {location.pathname === "/"&&(
-                <>
-                    <div className="container-fluid d-flex justify-content-between">
-                        <div>
-                            <Link to="/">
-                                <span className="navbar-brand m-1 h1">HOME</span>
-                            </Link>
-                        </div>
-                        <div className="m-1 w-50" style={{ position: "relative" }}>
-                            <form onSubmit={handleSearchSubmit}>
-                                <input
-                                    type="text"
-                                    className="form-control me-2"
-                                    placeholder="Buscar ingredientes..."
-                                    value={search}
-                                    onChange={handleSearchChange}
-                                />
-                                {renderDropdown()}
-                            </form>
-                        </div>
-                        <div className="ml-auto">
-                            {store.isLoggedIn && store.currentUser ? (
-                                <div className="dropdown">
-                                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                                        {store.currentUser.user_name || "error"}
-                                    </button>
-                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                        <li>
-                                            <Link className="dropdown-item" to={`/user-profile/${store.currentUser.id}`}>
-                                                    Perfil
-                                            </Link>
-                                        </li>
-                                        <li><a className="dropdown-item" href="#">Favoritos</a></li>
-                                        <li>
-                                            <Link className="dropdown-item" to={`/login`} onClick={actions.logout}>
-                                                    Logout
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                                ) : (
-                                <>
-                            <Link to="/login">
-                                <button className="btn btn-primary m-1">LOGIN</button>
-                            </Link>
-                            <Link to="/sign_in">
-                                <button className="btn btn-primary m-1">SIGN IN</button>
-                            </Link>
-                                </>
-                    )}
-                    </div>
-                    </div>
-                </>
-            )}       
-            
+                    </li>
+                </ul>
+            </div>
+        ) : (
+            <>
+                <Link to="/login">
+                    <button className="btn btn-primary m-1">LOGIN</button>
+                </Link>
+                <Link to="/sign_in">
+                    <button className="btn btn-primary m-1">SIGN IN</button>
+                </Link>
+            </>
+        )}
+    </div>
+</div>
 </nav>
 	);
 };
